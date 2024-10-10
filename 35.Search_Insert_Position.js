ee5112bf084a162784a55b79d99fe577c68e4b02
Artyom Shcherbina <scherbina.ar@gmail.com>
2024-10-10 22:04:14 +0200

    feat: add first task

---
+++ 35.Search_Insert_Position.js
@@ -0,0 +1,20 @@
+const searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        const middle = Math.floor((start + end) / 2);
+        if (target === nums[middle]) {
+            return middle;
+        } else if(nums[middle] > target) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
+
+    return start;
+};
+
+const result = searchInsert([2, 5, 6, 10, 12], 122);
+console.log(result);
